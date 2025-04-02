71d43516de48afab2306f95e185b72d93c3fc5a3
YaegakiY <lmrunowen@yahoo.co.jp>
2025-04-02 16:25:35 +0900

    about grover testing

--- dc_cond_gen/grover_generator_test.py
+++ dc_cond_gen/grover_generator_test.py
@@ -1,19 +1,28 @@
 from qiskit import transpile
 from qiskit_aer import Aer
 from gr_generator import generate_grover_subcircuit  # ← 你自己模块路径
+from qiskit import QuantumCircuit, ClassicalRegister
+from datetime import datetime
 
 def test_generate_grover_subcircuit(num_qubits=4, target_bitstring=None):
+    
+    ts = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    save_path = f"test_runs/{ts}_grover_{num_qubits}q.json"
+    
     # 获取已构建好的电路（已包含测量），其中电路对应频率最高的输出态
-    circuit, creg, target = generate_grover_subcircuit(num_qubits, target_bitstring)
+    qc, creg, target = generate_grover_subcircuit(
+        num_qubits=5,
+        save_info_to=save_path
+    )
 
     print("🎯 目标态应为:", target)
     print("📦 返回 ClassicalRegister 名称:", creg.name)
 
     print("【原始 Grover 电路】")
-    print(circuit.draw('text'))
+    print(qc.draw('text'))
 
     backend = Aer.get_backend("aer_simulator")
-    compiled = transpile(circuit, backend, optimization_level=3)
+    compiled = transpile(qc, backend, optimization_level=3)
 
     print("【优化后电路】")
     print(compiled.draw('text'))
