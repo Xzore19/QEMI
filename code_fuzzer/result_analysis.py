6a66b165c69e65baf2f0a6035d2dc591606dbef3
Xiaszore <xiaszore19@gmail.com>
2025-05-19 22:54:36 +0900

    update dead code generation and extra register

--- code_fuzzer/result_analysis.py
+++ code_fuzzer/result_analysis.py
@@ -1,6 +1,16 @@
 from scipy.stats import ks_2samp
 
+def remove_cr(dis, qnum):
+    cleaned_counts = {}
+    for k, v in dis.items():
+        bits = k.replace(" ", "")[-qnum:]  # 提取最后5位
+        cleaned_counts[bits] = cleaned_counts.get(bits, 0) + v
+    # print(cleaned_counts)
+    return cleaned_counts
+
 def probability_checker(truth, fuzzing, shot, qnum):
+    truth = remove_cr(truth, qnum)
+    fuzzing = remove_cr(fuzzing, qnum)
     # 对于两个概率分布结果进行误差分析
     check_list = [f'{num:0{qnum}b}' for num in range(pow(2, qnum))]
     truth_list = [truth.get(i, 0) for i in check_list]
@@ -11,6 +21,9 @@ def probability_checker(truth, fuzzing, shot, qnum):
     if stat > 0.2:
         print("stat:", stat)
         print("pvalue", pvalue)
+        print(truth)
+        print("=========================================")
+        print(fuzzing)
         return False
     else:
         return True
--- fuzzing/temp_test.py
+++ fuzzing/temp_test.py
@@ -11,103 +11,54 @@ from math import pi
 import numpy as np 
 np.random.seed(42) 
 
-qreg = QuantumRegister(7) 
+qreg = QuantumRegister(5) 
 creg = ClassicalRegister(5) 
-cond_creg = ClassicalRegister(2) 
-qc = QuantumCircuit(qreg, creg, cond_creg) 
+qc = QuantumCircuit(qreg, creg) 
 
-qc.append(U3Gate(2.554, 4.351, 0.736), [qreg[3]])
-qc.append(CUGate(2.352, 4.412, 4.133, 2.008), [qreg[3], qreg[2]])
-qc.append(HGate(), [qreg[3]])
-qc.append(C3XGate(), [qreg[1], qreg[2], qreg[4], qreg[0]])
-qc.rz(0.39269908169872414, 0)
-def create_oracle(qreg):
-    oracle = QuantumCircuit(qreg, name='Oracle')
-    oracle.x(qreg[0])
-    oracle.h(qreg[1])
-    oracle.mcx([qreg[0]], qreg[1])
-    oracle.h(qreg[1])
-    oracle.x(qreg[0])
-    return oracle.to_gate(label='Oracle')
+qc.append(TwoLocal(3, reps=1, parameter_prefix='theta_4bc563'), [qreg[2], qreg[4], qreg[0]])
+qc.ccz(4, 3, 2)
+qc.append(CUGate(3.769, 3.26, 6.08, 0.066), [qreg[3], qreg[2]])
+qc.cp(1.5707963267948966, 0, 1)
+aux_f7a897 = AncillaRegister(1, 'aux_f7a897')
+qc.add_register(aux_f7a897)
+qc.append(DraperQFTAdder(3), [qreg[3], qreg[0], aux_f7a897[0], qreg[2], qreg[4], qreg[1]])
 
-def create_diffuser(qreg):
-    diffuser = QuantumCircuit(qreg, name='Diffuser')
-    diffuser.h(qreg)
-    diffuser.x(qreg)
-    diffuser.h(qreg[1])
-    diffuser.mcx([qreg[0]], qreg[1])
-    diffuser.h(qreg[1])
-    diffuser.x(qreg)
-    diffuser.h(qreg)
-    return diffuser.to_gate(label='Diffuser')
-
-backend = Aer.get_backend('aer_simulator')
-oracle_gate = create_oracle(QuantumRegister(2))
-diffuser_gate = create_diffuser(QuantumRegister(2))
-results = []
-grdc_qreg = QuantumRegister(2)
-grdc_creg = ClassicalRegister(2)
-grdc_qc = QuantumCircuit(grdc_qreg, grdc_creg)
-grdc_qc.h(grdc_qreg)
-for _ in range(1):
-    grdc_qc.append(oracle_gate, qargs=grdc_qreg)
-    grdc_qc.append(diffuser_gate, qargs=grdc_qreg)
-
-qc.compose(grdc_qc, inplace = True, qubits = [5, 6]) 
-qc.measure(qreg[5], cond_creg[0]) 
-qc.measure(qreg[6], cond_creg[1]) 
-with qc.if_test((cond_creg, 0b10)) as else_1: 
-	def create_oracle(qreg):
-	    oracle = QuantumCircuit(qreg, name='Oracle')
-	    oracle.h(qreg[1])
-	    oracle.mcx([qreg[0]], qreg[1])
-	    oracle.h(qreg[1])
-	    return oracle.to_gate(label='Oracle')
+qr_7ab4bc = QuantumRegister(2)
+cr_7ab4bc = ClassicalRegister(2)
+qc.add_register(qr_7ab4bc)
+qc.add_register(cr_7ab4bc)
+qc.x(qr_7ab4bc[0])
+qc.x(qr_7ab4bc[1])
+qc.measure(qr_7ab4bc[0], cr_7ab4bc[0]) 
+qc.measure(qr_7ab4bc[1], cr_7ab4bc[1]) 
+with qc.if_test((cr_7ab4bc, 0b11)) as else_1: 
 	
-	def create_diffuser(qreg):
-	    diffuser = QuantumCircuit(qreg, name='Diffuser')
-	    diffuser.h(qreg)
-	    diffuser.x(qreg)
-	    diffuser.h(qreg[1])
-	    diffuser.mcx([qreg[0]], qreg[1])
-	    diffuser.h(qreg[1])
-	    diffuser.x(qreg)
-	    diffuser.h(qreg)
-	    return diffuser.to_gate(label='Diffuser')
-	
-	backend = Aer.get_backend('aer_simulator')
-	oracle_gate = create_oracle(QuantumRegister(2))
-	diffuser_gate = create_diffuser(QuantumRegister(2))
-	results = []
-	grdc_qreg = QuantumRegister(2)
-	grdc_creg = ClassicalRegister(2)
-	grdc_qc = QuantumCircuit(grdc_qreg, grdc_creg)
-	grdc_qc.h(grdc_qreg)
-	for _ in range(1):
-	    grdc_qc.append(oracle_gate, qargs=grdc_qreg)
-	    grdc_qc.append(diffuser_gate, qargs=grdc_qreg)
-	
-	qc.compose(grdc_qc, inplace = True, qubits = [5, 6]) 
-	qc.measure(qreg[5], cond_creg[0]) 
-	qc.measure(qreg[6], cond_creg[1]) 
-	with qc.while_loop((cond_creg, 0b10)): 
-		qc.append(C3XGate(), [qreg[4], qreg[1], qreg[0], qreg[3]])
-		qc.append(StatePreparation([(-0.15089067187235183+0.1289695951511149j), (0.2550918869044753-0.051005848384255356j), (0.16623034720167806+0.07230054570594342j), (-0.07703798767869245-0.09788096289004322j), (-0.028436403639503786+0.12714016505394762j), (0.07478183796445821+0.13936842691390083j), (-0.06437693945481603-0.19885321854998272j), (0.08230890891263634-0.1787372875832085j), (-0.13057978026372405+0.09740629643440635j), (-0.029122156732678046+0.11880616920035547j), (-0.06080670920972035+0.008336361260606202j), (0.21504493068465247+0.05040294514609499j), (-0.04123034688894584-0.20857935235005956j), (-0.22761332781890792-0.08248688943826671j), (0.15330686587507739+0.09923857405376264j), (0.032597721468844126-0.08665292784129765j), (-0.0885148783293555-0.1557006788914944j), (-0.06608265189108486-0.14055844157604777j), (0.16474986465303254-0.09901872565750869j), (-0.12187766388994876+0.09564467710840807j), (-0.09649950235710782+0.0428185845545461j), (-0.08156301882771276-0.05557320155995496j), (0.02722354316231862-0.08539233896473909j), (0.1627771758687312-0.006703346295594797j), (0.13704466127029902-0.07894063910008539j), (-0.12463398993568613+0.1157846781330754j), (0.04881027950224188+0.12577628014125936j), (-0.0034445932491116962-0.14906213786928377j), (0.19069006390804244-0.05053392397847821j), (-0.05102654611786124-0.26018107266782153j), (-0.25880477586360723-0.14105724666808794j), (0.027082472090048756+0.12859334983738496j)]), [qreg[3], qreg[0], qreg[1], qreg[4], qreg[2]])
-		qc.append(ZFeatureMap(1, reps=1, parameter_prefix='x_09ce98'), [qreg[1]])
-		qc.ccx(3, 2, 1)
-		qc.append(C3XGate(), [qreg[4], qreg[3], qreg[2], qreg[1]])
-		qc.measure(qreg[5], cond_creg[0]) 
-		qc.measure(qreg[6], cond_creg[1]) 
+	qr_52cca2 = QuantumRegister(2)
+	cr_52cca2 = ClassicalRegister(2)
+	qc.add_register(qr_52cca2)
+	qc.add_register(cr_52cca2)
+	qc.x(qr_52cca2[0])
+	qc.x(qr_52cca2[1])
+	qc.measure(qr_52cca2[0], cr_52cca2[0]) 
+	qc.measure(qr_52cca2[1], cr_52cca2[1]) 
+	with qc.while_loop((cr_52cca2, 0b10)): 
+		qc.append(C3XGate(), [qreg[3], qreg[1], qreg[0], qreg[2]])
+		qc.append(CXGate(), [qreg[4], qreg[3]])
+		qc.iswap(2, 1)
+		qc.append(Permutation(5, pattern=[2, 3, 1, 4, 0]), [qreg[2], qreg[1], qreg[4], qreg[3], qreg[0]])
+		qc.append(RZGate(2.186), [qreg[4]])
+		qc.measure(qr_52cca2[0], cr_52cca2[0]) 
+		qc.measure(qr_52cca2[1], cr_52cca2[1]) 
 	
 	
 with else_1: 
 	pass 
 
-qc.append(XOR(2), [qreg[3], qreg[0]])
-qc.append(CCXGate(), [qreg[4], qreg[0], qreg[1]])
-qc.cswap(3, 1, 0)
-qc.append(QFT(1), [qreg[4]])
-qc.append(Permutation(1, pattern=[0]), [qreg[0]])
+qc.append(XGate(), [qreg[4]])
+qc.append(Isometry(np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]), 0, 0), [qreg[3], qreg[0], qreg[1]])
+qc.append(HGate(), [qreg[3]])
+qc.append(EfficientSU2(2, reps=1, parameter_prefix='theta_d8069f'), [qreg[4], qreg[3]])
+qc.append(CRXGate(0.125), [qreg[4], qreg[2]])
 qc.measure(qreg[0], creg[0]) 
 qc.measure(qreg[1], creg[1]) 
 qc.measure(qreg[2], creg[2]) 
@@ -119,7 +70,7 @@ qc = qc.assign_parameters({p: np.random.uniform(0, 2 * np.pi) for p in qc.parame
 
 simulator = Aer.get_backend("aer_simulator") 
 
-p = PassManager(Optimize1qGates()) 
+p = PassManager(ConsolidateBlocks()) 
 qc = p.run(qc) 
 
 compiled_circuit = transpile(qc, backend = simulator, optimization_level = 3, routing_method = "default", layout_method = "noise_adaptive", approximation_degree = 1 ) 
