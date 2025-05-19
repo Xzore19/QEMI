9a0b6e86a49c25c81979f20a676ed7806f013bc9
Xiaszore <xiaszore19@gmail.com>
2025-05-20 00:21:22 +0900

    update XOR seed=42

--- qiskit_api.py
+++ qiskit_api.py
@@ -134,7 +134,7 @@ def generate_dynamic_gate(gate_cls, requested_qubits):
     elif gate_cls in [OR, AND, XOR]:
         num_variable_qubits = requested_qubits - 1
         gate = gate_cls(num_variable_qubits)
-        gate._custom_expr = f"{gate_cls.__name__}({num_variable_qubits})"
+        gate._custom_expr = f"{gate_cls.__name__}({num_variable_qubits}, seed=42)"
         return gate, gate.num_qubits
 
     else:
