a9978da4b769c3ae2697ed3641569d057607e017
Xiaszore <xiaszore19@gmail.com>
2025-06-03 16:22:10 +0900

    qasm checker

--- qasm_code/buggy_program/crash/fuzzing_0.py
+++ qasm_code/buggy_program/crash/fuzzing_0.py
@@ -1,57 +1,21 @@
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile, AncillaRegister 
-from qiskit_aer import Aer 
-from qiskit.providers.fake_provider import GenericBackendV2 
-from qiskit.providers.fake_provider import GenericBackendV2 
-from qiskit.circuit import Parameter, ParameterVector 
-from qiskit.circuit.library import XGate 
-from qiskit.transpiler.passes import * 
-from qiskit.circuit.library import * 
-from qiskit.transpiler import PassManager, generate_preset_pass_manager 
-from math import pi 
-import numpy as np 
-np.random.seed(42) 
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.qasm3 import dumps
 
-qreg = QuantumRegister(5) 
-creg = ClassicalRegister(5) 
+qreg = QuantumRegister(1)
+creg = ClassicalRegister(1)
 qc = QuantumCircuit(qreg, creg) 
 
-qc.swap(2, 1)
-qc.ch(2, 0)
-qc.y(4)
-qc.t(0)
-qc.swap(3, 2)
+for i in range(3):
+	qc.h(0)
 
-qr_8cf2c4 = QuantumRegister(2)
-cr_8cf2c4 = ClassicalRegister(2)
-qc.add_register(qr_8cf2c4)
-qc.add_register(cr_8cf2c4)
-qc.x(qr_8cf2c4[0])
-qc.x(qr_8cf2c4[1])
-qc.measure(qr_8cf2c4[0], cr_8cf2c4[0]) 
-qc.measure(qr_8cf2c4[1], cr_8cf2c4[1]) 
-with qc.while_loop((cr_8cf2c4, 0b10)): 
-	a = 0
-	with qc.for_loop(range(a)) as i_1b1d12:
-		qc.cswap(4, 2, 1)
-		qc.swap(1, 0)
-		qc.rz(0.39269908169872414, 0)
-		qc.rx(0.7853981633974483, 1)
-		qc.z(3)
-	
-	qc.measure(qr_8cf2c4[0], cr_8cf2c4[0]) 
-	qc.measure(qr_8cf2c4[1], cr_8cf2c4[1]) 
-qc.reset(qr_8cf2c4)
-qc.cz(0, 3)
-qc.ch(2, 0)
-qc.rx(1.5707963267948966, 1)
-qc.cx(2, 0)
-qc.h(4)
-qc.measure(qreg[0], creg[0]) 
-qc.measure(qreg[1], creg[1]) 
-qc.measure(qreg[2], creg[2]) 
-qc.measure(qreg[3], creg[3]) 
-qc.measure(qreg[4], creg[4]) 
+with qc.for_loop(range(3)) as i:
+	qc.z(0)
 
-qc = qc.assign_parameters({p: 0.5 for p in qc.parameters})
+qc.measure(qreg[0], creg[0])
+
+qasm3_code = dumps(qc)
+print(qasm3_code)
+
+
 
 
