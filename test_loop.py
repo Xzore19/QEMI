34eddd47d58c20efe6948fa648def41084885bde
YaegakiY <lmrunowen@yahoo.co.jp>
2025-07-16 17:48:00 +0900

    nothing

--- test_loop.py
+++ test_loop.py
@@ -1,44 +1,41 @@
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
-from qiskit_aer import Aer
-from qiskit.transpiler.passes import *
-from qiskit.circuit.library import *
-from qiskit.transpiler import PassManager
-import numpy as np
+import cirq
+from cirq.transformers import *
 
-np.random.seed(42)
+q = cirq.LineQubit.range(2)
+circuit = cirq.Circuit()
 
-qreg = QuantumRegister(4)
-creg = ClassicalRegister(4)
-qc = QuantumCircuit(qreg, creg)
+circuit.append(cirq.H(q[1]))
 
-qc.ch(2, 1) # this line
-qc.z(2) # this line
-qc.rx(0.39269908169872414, 0) # this line
-qc.ch(3, 0) # this line
-qc.crx(0.7853981633974483, 0, 3) # this line
+circuit.append(cirq.H(q[0]).controlled_by(q[1]))
 
-qr = QuantumRegister(2)
-cr = ClassicalRegister(2)
-qc.add_register(qr)
-qc.add_register(cr)
-qc.h(qr[0]) 
-qc.cx(qr[0], qr[1]) 
-qc.measure(qr[0], cr[0])
-qc.measure(qr[1], cr[1])
+circuit.append(cirq.T(q[1]))
+circuit.append(cirq.H(q[1]))
 
-qc.measure(qreg[0], creg[0])
-qc.measure(qreg[1], creg[1])
-qc.measure(qreg[2], creg[2])
-qc.measure(qreg[3], creg[3])
-
-simulator = Aer.get_backend("aer_simulator")
-
-p = PassManager([Collect2qBlocks(), TemplateOptimization(), ConsolidateBlocks()])
-qc = p.run(qc)
-
-compiled_circuit = transpile(qc, backend=simulator)
+# circuit.append(cirq.measure(q, key="m"))
 
 
-job = simulator.run(compiled_circuit, shots=500)
-result = job.result().get_counts()
-print(result)
+circuit = expand_composite(circuit)
+circuit = merge_k_qubit_unitaries(circuit, k=2)
+circuit = optimize_for_target_gateset(circuit)
+
+p = cirq.LineQubit.range(2)
+circuit2 = cirq.Circuit()
+
+circuit2.append(cirq.H(p[1]))
+
+circuit2.append(cirq.H(p[0]).controlled_by(p[1]))
+
+circuit2.append(cirq.T(p[1]))
+circuit2.append(cirq.H(p[1]))
+
+# circuit2.append(cirq.measure(q, key="m"))
+
+# simulator = cirq.Simulator()
+# result = simulator.run(circuit, repetitions=10000)
+# print(result.histogram(key='m'))
+
+# result = simulator.simulate(circuit)
+
+# print(result.final_state_vector)
+
+print(cirq.equal_up_to_global_phase(cirq.unitary(circuit), cirq.unitary(circuit2)))
