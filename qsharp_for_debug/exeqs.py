e99eecf4bb37d8bb95ed3b5b852d134f93780d42
YaegakiY <lmrunowen@yahoo.co.jp>
2025-07-19 15:09:38 +0900

    delete the trash

--- qsharp_for_debug/exeqs.py
+++
@@ -1,5 +0,0 @@
-import qsharp
-
-qsharp.init(project_root=".")
-
-print(qsharp.eval("Main.Main()"))
--- test_fuzzing_test.py
+++
@@ -1,7 +0,0 @@
-# test_fuzzing_test.py
-from fuzzing import fuzzing_test
-
-def test_run():
-    result = fuzzing_test.run()
-    assert isinstance(result, dict)
-    assert sum(result.values()) == 1  # shots=1
